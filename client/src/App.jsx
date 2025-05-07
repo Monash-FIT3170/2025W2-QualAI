c903b7d0318e51d518a12d75be2478b44417039c
Mohidkz05 <mkha0155@student.monash.edu>
2025-05-07 14:59:43 +1000

    Configured tailwind.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,6 +8,9 @@ function App() {
 
   return (
     <>
+      <div className='bg-violet-400'>
+        <p>dekjdma</p>
+      </div>
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
