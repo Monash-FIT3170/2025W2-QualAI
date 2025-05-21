0bbde347719c0d592dc152580b0f77156d2fdafa
Gunnraj98 <gdha0007@student.monash.edu>
2025-05-21 22:58:29 +1000

    Fix: removed exception catching in database models, and added new exception raising behaviour in the event of a get or delete that has an id with no corresponding entry in database

--- server/app/database_models.py
+++ server/app/database_models.py
@@ -4,6 +4,7 @@ from typing import Optional
 PROJECT_TABLE_NAME = "project"
 TRANS_TABLE_NAME = "transcription"
 
+
 class Project:
     """
     A class that allows for interaction with the projects in the database
@@ -15,30 +16,25 @@ class Project:
         """
         self.db_name = database_name
         self._create_table()
-        
 
     def _create_table(self) -> None:
         """
         Private method. Creates the project table if it does not yet exist, otherwise does nothing.
         """
-        try:
-            with sqlite3.connect(self.db_name) as conn:
-                cur = conn.cursor()
-                cur.execute("PRAGMA foreign_keys = ON;")
+        with sqlite3.connect(self.db_name) as conn:
+            cur = conn.cursor()
+            cur.execute("PRAGMA foreign_keys = ON;")
 
-                cur.execute(f"""
-                    CREATE TABLE IF NOT EXISTS {PROJECT_TABLE_NAME} (
-                        project_id INTEGER PRIMARY KEY AUTOINCREMENT,
-                        name TEXT NOT NULL UNIQUE,
-                        description TEXT NOT NULL,
-                        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-                    )
-                """)
+            cur.execute(f"""
+                CREATE TABLE IF NOT EXISTS {PROJECT_TABLE_NAME} (
+                    project_id INTEGER PRIMARY KEY AUTOINCREMENT,
+                    name TEXT NOT NULL UNIQUE,
+                    description TEXT NOT NULL,
+                    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+                )
+            """)
 
-        except sqlite3.Error as e:
-            print(f"There was an error creating the table: {e}")
-
-    def insert(self, project_name: str, description: str = "") -> Optional[int]:
+    def insert(self, project_name: str, description: str = "") -> int:
         """
         Method to insert propject into the table. Returns the id of the project on success, and None on failure.
 
@@ -47,54 +43,48 @@ class Project:
             description (str): The description of the project
 
         Returns:
-            Optional int: The project id on successful insert, or None on failure.
+            int: The project id on successful insert, or None on failure.
         """
-        try:
-            project_id = None
+        project_id = None
 
-            with sqlite3.connect(self.db_name) as conn:
-                cur = conn.cursor()
-                cur.execute("PRAGMA foreign_keys = ON;")
+        with sqlite3.connect(self.db_name) as conn:
+            cur = conn.cursor()
+            cur.execute("PRAGMA foreign_keys = ON;")
 
-                cur.execute(f"""
-                    INSERT INTO {PROJECT_TABLE_NAME} (name, description) VALUES (?, ?)
-                """, (project_name, description))
+            cur.execute(f"""
+                INSERT INTO {PROJECT_TABLE_NAME} (name, description) VALUES (?, ?)
+            """, (project_name, description))
 
-                project_id =  cur.lastrowid
+            project_id = cur.lastrowid
 
-            return project_id
+        return project_id
 
-        except sqlite3.Error as e:
-            print(f"There was an error inserting into the table: {e}")
-            return None
-
-    def delete(self, project_id: int) -> bool:
+    def delete(self, project_id: int):
         """
         Method to delete project from table. Will also delete all associated transcriptions
 
         Args:
             project_id (int): project id to delete
 
-        Returns:
-            bool: True on successful delete, False on failure
+        Raises:
+            ValueError: If there is no row associated with the project id, then a ValueError is raised
         """
-        try:
-            with sqlite3.connect(self.db_name) as conn:
-                cur = conn.cursor()
-                cur.execute("PRAGMA foreign_keys = ON;")
+        rows_deleted = 0
+        with sqlite3.connect(self.db_name) as conn:
+            cur = conn.cursor()
+            cur.execute("PRAGMA foreign_keys = ON;")
 
-                cur.execute(f"""
-                    DELETE FROM {PROJECT_TABLE_NAME}
-                    WHERE project_id = ? 
-                """, (project_id,))
+            cur.execute(f"""
+                DELETE FROM {PROJECT_TABLE_NAME}
+                WHERE project_id = ? 
+            """, (project_id,))
 
-            return True
+            rows_deleted = cur.rowcount
 
-        except sqlite3.Error as e:
-            print(f"There was an error deleting the project: {e}")
-            return False
-    
-    def get_project_by_id(self, project_id: int) -> Optional[tuple[str, str, str]]:
+        if rows_deleted < 1:
+            raise ValueError("There is no row associated with this project id!")
+
+    def get_project_by_id(self, project_id: int) -> tuple[str, str, str]:
         """
         Method to get project data by its id.
 
@@ -102,73 +92,49 @@ class Project:
             transcription_id (int): The id of the project
 
         Returns:
-            Optional tuple[str, str, str]: The project name, description and creation date
+            tuple[str, str, str]: The project name, description and creation date
+
+        Raises:
+            LookupError: If there is no project with the given id, raises LookupError
         """
-        try:
-            project = None
+        project = None
 
-            with sqlite3.connect(self.db_name) as conn:
-                cur = conn.cursor()
-                cur.execute("PRAGMA foreign_keys = ON;")
+        with sqlite3.connect(self.db_name) as conn:
+            cur = conn.cursor()
+            cur.execute("PRAGMA foreign_keys = ON;")
 
-                cur.execute(f"""
-                    SELECT name, description, created_at FROM {PROJECT_TABLE_NAME}
-                    WHERE project_id = ?
-                """, (project_id,))
+            cur.execute(f"""
+                SELECT name, description, created_at FROM {PROJECT_TABLE_NAME}
+                WHERE project_id = ?
+            """, (project_id,))
 
-                project = cur.fetchone()
+            project = cur.fetchone()
 
-            return project
+        if project is None:
+            raise LookupError("There is no project with that id!")
 
-        except sqlite3.Error as e:
-            print(f"There was an error getting the project: {e}")
-            return None
+        return project
 
-    def get_all_projects(self) -> Optional[list[tuple[int, str, str, str]]]:
+    def get_all_projects(self) -> list[tuple[int, str, str, str]]:
         """
         Method to get all projects that exist.
 
         Returns:
-            Optional list[tuple[int, str, str, str]]: The project id, name description and creation date for each project
+            list[tuple[int, str, str, str]]: The project id, name description and creation date for each project
         """
-        try:
-            projects = None
+        projects = None
 
-            with sqlite3.connect(self.db_name) as conn:
-                cur = conn.cursor()
-                cur.execute("PRAGMA foreign_keys = ON;")
+        with sqlite3.connect(self.db_name) as conn:
+            cur = conn.cursor()
+            cur.execute("PRAGMA foreign_keys = ON;")
 
-                cur.execute(f"""
-                    SELECT project_id, name, description, created_at FROM {PROJECT_TABLE_NAME}
-                """)
+            cur.execute(f"""
+                SELECT project_id, name, description, created_at FROM {PROJECT_TABLE_NAME}
+            """)
 
-                projects = cur.fetchall()
+            projects = cur.fetchall()
 
-            return projects
-
-        except sqlite3.Error as e:
-            print(f"There was an error getting the projects: {e}")
-            return None
-        
-    def drop_table(self):
-        """
-        Dev method, should not be kept in final version!!
-        """
-        try:
-            with sqlite3.connect(self.db_name) as conn:
-                cur = conn.cursor()
-                cur.execute("PRAGMA foreign_keys = ON;")
-
-                cur.execute(f"""
-                    DROP TABLE IF EXISTS {PROJECT_TABLE_NAME};
-                """)
-
-            return None
-
-        except sqlite3.Error as e:
-            print(f"There was an error getting the projects: {e}")
-            return None
-
+        return projects
 
 
 class Transcription:
@@ -182,29 +148,25 @@ class Transcription:
         """
         self.db_name = database_name
         self._create_table()
-        
 
     def _create_table(self) -> None:
         """
         Private method. Creates the transcription table if it does not yet exist, otherwise does nothing.
         """
-        try:
-            with sqlite3.connect(self.db_name) as conn:
-                cur = conn.cursor()
-                cur.execute("PRAGMA foreign_keys = ON;")
+        with sqlite3.connect(self.db_name) as conn:
+            cur = conn.cursor()
+            cur.execute("PRAGMA foreign_keys = ON;")
 
-                cur.execute(f"""
-                    CREATE TABLE IF NOT EXISTS {TRANS_TABLE_NAME} (
-                        transcription_id INTEGER PRIMARY KEY AUTOINCREMENT,
-                        project_id INTEGER,
-                        name TEXT NOT NULL,
-                        transcription TEXT NOT NULL,
-                        processed_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-                        FOREIGN KEY (project_id) REFERENCES {PROJECT_TABLE_NAME}(project_id) ON DELETE CASCADE
-                    )
-                """)
-        except sqlite3.Error as e:
-            print(f"There was an error creating the table: {e}")
+            cur.execute(f"""
+                CREATE TABLE IF NOT EXISTS {TRANS_TABLE_NAME} (
+                    transcription_id INTEGER PRIMARY KEY AUTOINCREMENT,
+                    project_id INTEGER,
+                    name TEXT NOT NULL,
+                    transcription TEXT NOT NULL,
+                    processed_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+                    FOREIGN KEY (project_id) REFERENCES {PROJECT_TABLE_NAME}(project_id) ON DELETE CASCADE
+                )
+            """)
 
     def insert(self, project_id: int, name: str, transcription: str) -> Optional[int]:
         """
@@ -216,54 +178,48 @@ class Transcription:
             transcription (str): The transcription for the audio
 
         Returns:
-            Optional int: The transcription id on successful insert, or None on failure.
+            int: The transcription id on successful insert, or None on failure.
         """
-        try:
-            transcription_id = None
+        transcription_id = None
 
-            with sqlite3.connect(self.db_name) as conn:
-                cur = conn.cursor()
-                cur.execute("PRAGMA foreign_keys = ON;")
+        with sqlite3.connect(self.db_name) as conn:
+            cur = conn.cursor()
+            cur.execute("PRAGMA foreign_keys = ON;")
 
-                cur.execute(f"""
-                    INSERT INTO {TRANS_TABLE_NAME} (project_id, name, transcription) VALUES (?, ?, ?)
-                """, (project_id, name, transcription))
+            cur.execute(f"""
+                INSERT INTO {TRANS_TABLE_NAME} (project_id, name, transcription) VALUES (?, ?, ?)
+            """, (project_id, name, transcription))
 
-                transcription_id = cur.lastrowid 
+            transcription_id = cur.lastrowid
 
-            return transcription_id
+        return transcription_id
 
-        except sqlite3.Error as e:
-            print(f"There was an error inserting into the table: {e}")
-            return None
-        
-    def delete(self, transcription_id: int) -> bool:
+    def delete(self, transcription_id: int):
         """
         Method to delete transcription from table.
 
         Args:
             transcription_id (int): Transcription id to delete
 
-        Returns:
-            bool: True on successful delete, False on failure
+        Raises:
+            ValueError: If there is no row associated with the transcription id, raises ValueError
         """
-        try:
-            with sqlite3.connect(self.db_name) as conn:
-                cur = conn.cursor()
-                cur.execute("PRAGMA foreign_keys = ON;")
+        rows_deleted = 0
+        with sqlite3.connect(self.db_name) as conn:
+            cur = conn.cursor()
+            cur.execute("PRAGMA foreign_keys = ON;")
 
-                cur.execute(f"""
-                    DELETE FROM {TRANS_TABLE_NAME}
-                    WHERE transcription_id = ?
-                """, (transcription_id,))
+            cur.execute(f"""
+                DELETE FROM {TRANS_TABLE_NAME}
+                WHERE transcription_id = ?
+            """, (transcription_id,))
 
-            return True
+            rows_deleted = cur.rowcount
 
-        except sqlite3.Error as e:
-            print(f"There was an error deleting the transcription: {e}")
-            return False
-        
-    def get_transcription_by_id(self, transcription_id: int) -> Optional[tuple[int, str, str, str]]:
+        if rows_deleted < 1:
+            raise ValueError("There is no row associated with this transcription id!")
+
+    def get_transcription_by_id(self, transcription_id: int) -> tuple[int, str, str, str]:
         """
         Method to get transcriptions data by its id.
 
@@ -271,28 +227,27 @@ class Transcription:
             transcription_id (int): The id of the transcription
 
         Returns:
-            Optional tuple[int, str, str, str]: The transcription id, name, transcription text and process date
+            tuple[int, str, str, str]: The transcription id, name, transcription text and process date
         """
-        try:
-            transcription = None
+        transcription = None
 
-            with sqlite3.connect(self.db_name) as conn:
-                cur = conn.cursor()
-                cur.execute("PRAGMA foreign_keys = ON;")
+        with sqlite3.connect(self.db_name) as conn:
+            cur = conn.cursor()
+            cur.execute("PRAGMA foreign_keys = ON;")
 
-                cur.execute(f"""
-                    SELECT project_id, name, transcription, processed_at FROM {TRANS_TABLE_NAME}
-                    WHERE transcription_id = ?
-                """, (transcription_id,))
+            cur.execute(f"""
+                SELECT project_id, name, transcription, processed_at FROM {TRANS_TABLE_NAME}
+                WHERE transcription_id = ?
+            """, (transcription_id,))
 
-                transcription = cur.fetchone()
+            transcription = cur.fetchone()
 
-            return transcription
+        if transcription is None:
+            raise LookupError("There is no transcription with that id!")
 
-        except sqlite3.Error as e:
-            print(f"There was an error getting the transcription: {e}")
+        return transcription
 
-    def get_all_project_transcriptions(self, project_id: int) -> Optional[list[tuple[int, str, str]]]:
+    def get_all_project_transcriptions(self, project_id: int) -> list[tuple[int, str, str]]:
         """
         Method to get all transcriptions associated with a project. Crucially, it does not return the transcription text itself.
 
@@ -300,43 +255,30 @@ class Transcription:
             project_id (int): The id of the project
 
         Returns:
-            Optional list[tuple[int, str, str]]: The transcription id, name and process date for each transcription associated with the project
+            list[tuple[int, str, str]]: The transcription id, name and process date for each transcription associated with the project
         """
-        try:
-            transcriptions = None
+        transcriptions = None
 
-            with sqlite3.connect(self.db_name) as conn:
-                cur = conn.cursor()
-                cur.execute("PRAGMA foreign_keys = ON;")
+        with sqlite3.connect(self.db_name) as conn:
+            cur = conn.cursor()
+            cur.execute("PRAGMA foreign_keys = ON;")
 
-                cur.execute(f"""
-                    SELECT transcription_id, name, processed_at FROM {TRANS_TABLE_NAME}
-                    WHERE project_id = ?
-                """, (project_id,))
+            cur.execute(f"""
+                SELECT transcription_id, name, processed_at FROM {TRANS_TABLE_NAME}
+                WHERE project_id = ?
+            """, (project_id,))
 
-                transcriptions = cur.fetchall()
+            transcriptions = cur.fetchall()
 
-            return transcriptions
+        return transcriptions
+    
+if __name__ == "__main__":
+    project_manager = Project("qualAI_test.db")
+    project_name = "unique_project_4"
+    inserted_id = project_manager.insert(
+        project_name, "To be deleted")
+    print(inserted_id)
+    deletion_successful = project_manager.delete(inserted_id)
+    print(deletion_successful)
 
-        except sqlite3.Error as e:
-            print(f"There was an error getting the project transcriptions: {e}")
-
-    def drop_table(self):
-        """
-        Dev method, should not be kept in final version!!
-        """
-        try:
-            with sqlite3.connect(self.db_name) as conn:
-                cur = conn.cursor()
-                cur.execute("PRAGMA foreign_keys = ON;")
-
-                cur.execute(f"""
-                    DROP TABLE {TRANS_TABLE_NAME};
-                """)
-
-            return None
-
-        except sqlite3.Error as e:
-            print(f"There was an error getting the projects: {e}")
-            return None
-        
+    project_manager.get_project_by_id(inserted_id)
